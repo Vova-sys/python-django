0a7e5f65075ea54185472463bd1663a25ccf355b
v.dedkov <d3dkof@gmail.com>
2020-10-07 16:48:10 +0300

    deploy heroku

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -94,13 +94,15 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': 5432
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, "db.sqlite3"),
 
+        #'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       # 'NAME': os.environ.get('NAME'),
+       # 'USER': os.environ.get('USER'),
+       # 'PASSWORD': os.environ.get('PASSWORD'),
+      #  'HOST': os.environ.get('HOST'),
+       # 'PORT': 5432
     }
 }
 
