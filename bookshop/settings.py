77e9ceb809c69bc90c28aabad6abd550ee96391a
Vova-sys <d3dkof@gmail.com>
2022-02-25 16:15:47 +0100

    configuration2

--- bookshop/settings.py
+++ bookshop/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'bookshop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql.psycopg2',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
         'PASSWORD': os.environ.get('PASSWORD'),
